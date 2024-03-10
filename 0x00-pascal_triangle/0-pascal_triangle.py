3f856e1fbf0e819dbf42fa9079b291baa73cc0c8
tommy457 <boukhalfaml1011@gmail.com>
2024-03-10 14:24:38 +0000

    0. Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module for Pascal’s triangle Algorithm implementation"""
+
+def pascal_triangle(n):
+    """Create list of lists of integers representing the Pascal’s triangle"""
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        if i == 0:
+            triangle.append([1])
+        else:
+            new_row = []
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    new_row.append(1)
+                else:
+                    new_row.append(triangle[i - 1][j] + triangle[i - 1][j - 1])
+            triangle.append(new_row)
+
+    return triangle
