b74ad2809d3f3a98adf529475156da091061a0dc
tommy457 <boukhalfaml1011@gmail.com>
2024-03-28 17:25:39 +0000

    Log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Script that reads stdin line by line and computes metrics.
+"""
+import sys
+import re
+from typing import Dict
+
+
+pattern = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[([\d-]+\s[\d:.]+)\] "GET /projects/260 HTTP/1.1" (?P<status_code>\d{3}) (?P<file_size>\d+)'
+counter = 0
+results = {}
+total_file_size = 0
+
+def print_stats(stats: Dict[int, str], total_file_size: int) -> None:
+    """ print all items in a dictionary. """
+    print("File size:", total_file_size)
+
+    for key, value in stats:
+        print("{}: {}".format(key, value))
+
+try:
+    for line in sys.stdin:
+        counter += 1
+        pattern_match = re.match(pattern, line)
+        status_code = pattern_match.group("status_code")
+
+        try:
+            status_code = int(status_code)
+        except ValueError:
+            status_code = ""
+
+        if pattern_match and status_code:
+            total_file_size += int(pattern_match.group("file_size"))
+            results[status_code] = results.get(status_code, 0) + 1
+
+        if counter == 10:
+            print_stats(sorted(results.items()), total_file_size)
+            counter = 0
+
+    print_stats(sorted(results.items()), total_file_size)
+
+finally:
+    print_stats(sorted(results.items()), total_file_size)
