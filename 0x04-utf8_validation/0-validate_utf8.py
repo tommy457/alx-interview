4a66fff281814ad3d84424fa6e3f81a934d52afc
tommt457 <boukhalfaml1011@gmail.com>
2024-04-04 17:05:13 +0000

    refactor using bitwise operations

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,26 +2,21 @@
 """
 Module for the validUTF8 function.
 """
+from typing import List
 
 
-def validUTF8(data):
-    """ check if data set represents a valid UTF-8 encoding. """
-
+def validUTF8(data: List[int]) -> bool:
     i = 0
     while i < len(data):
-        leading_byte: str = f"{data[i]:08b}"
-        if leading_byte.startswith("0"):
+        leading_byte = data[i]
+        if leading_byte >> 7 & 1 == 0:
             bytes = 1
-
-        elif leading_byte.startswith("110"):
+        elif leading_byte >> 5 & 1 == 0:
             bytes = 2
-
-        elif leading_byte.startswith("1110"):
+        elif leading_byte >> 4 & 1 == 0:
             bytes = 3
-
-        elif leading_byte.startswith("11110"):
+        elif leading_byte >> 3 & 1 == 0:
             bytes = 4
-
         else:
             return False
 
@@ -29,9 +24,8 @@ def validUTF8(data):
             return False
 
         for j in range(1, bytes):
-            if not f"{data[i + j]:08b}".startswith("10"):
+            if (data[i + j] >> 6 & 1) != 0:
                 return False
 
         i += bytes
-
     return True
